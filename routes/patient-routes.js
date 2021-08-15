2ecb8d81770a484b6a118bc30961438905b58b65
Irene Suarez <irenesuarezalvarez@gmail.com>
2021-08-15 22:19:21 +0200

    Create new patient fixed and delete option working for the patient, not checked if deleted from professionals array

--- routes/patient-routes.js
+++ routes/patient-routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Patient = require('../models/Patient.model');
 const Professional = require('../models/Professional.model');
+
 //CREATE DATA ROUTE
 router.post('/create', (req, res, next) => {
     const { name, surname, email, phone, address, city, state, postal, contactname, contactsurname, contactemail, contactphone, professional, history } = req.body;
@@ -19,7 +20,16 @@ router.post('/create', (req, res, next) => {
 })
 
 //SEND DATA LIST OF PATIENTS
+//ALL PATIENTS
+router.get('/all', (req, res, next) => {
+    Patient.find()
+        .then((patientsFromDb) =>{
+            res.status(200).json(patientsFromDb);
+        })
+        .catch(error => console.log(`Error while searching a new patient:`, error));
+})
 
+//ALL PATIENTS FROM ONE PROFESSIONAL
 router.get('/', (req, res, next) => {
     console.log('PATIENTS API REQ SESS',req.session)
     Professional.find()
@@ -28,4 +38,35 @@ router.get('/', (req, res, next) => {
         })
         .catch(error => console.log(`Error while creating a new patient:`, error));
 })
+
+//DELETE PATIENT
+router.delete('/all/:id', (req, res, next) => {
+    const { id } = req.params;
+    Professional.find()
+        .populate('patients')
+        .then(profs =>  {
+                $pull: {patients : id}
+        })
+    Patient.findByIdAndDelete(id)
+        .then((patientFromDb) =>{
+            res.status(200).json(patientFromDb);
+        })
+        .catch(error => console.log(`Error while deleting the patient:`, error));
+})
+//Model from module 2, maybe usefull for later
+/* router.delete('/:id/delete', async (req, res) => {
+    const { id } = req.params;
+    try{
+      const patientArray = await Professional.findByIdAndUpdate(req.session.user._id, {
+          $pull: { patients: id },
+      });
+      
+      const deletePatient = await Patient.findByIdAndDelete(id)
+      return res.redirect('/patients')
+    }
+    catch(error) {
+      next(error)
+    }
+  }); */
+
 module.exports = router;
