e6c80d2f6af611a31a2174cd014e9813e9b2e57f
Irene Suarez <irenesuarezalvarez@gmail.com>
2021-08-28 18:04:57 +0200

    Changed origin in app

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 /* app.use(cookieParser()); */
 app.use(cors({ // to enable the server receive data
   credentials: true,
-  origin: ["http://localhost:3000"]
+  origin: process.env.CLIENT_URL
 }))
 
 
